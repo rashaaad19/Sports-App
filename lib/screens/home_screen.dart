c6eb5635d195a16cbc9b451a7a033e362ca856c8
rashaaad19 <egyoussef_2000@hotmail.com>
2025-05-27 01:18:38 +0300

    feat: added logout functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sportsapp/screens/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  void _logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+
+    //* navigate back to login screen
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +28,18 @@ class HomeScreen extends StatelessWidget {
       print("No user is currently signed in.");
     }
 
-    return Scaffold(body: Center(child: Text('Home Screeen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+            tooltip: 'Logout',
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Home Screeen')),
+    );
   }
 }
