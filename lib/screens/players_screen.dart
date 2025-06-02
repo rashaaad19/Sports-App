39d76fec250b713b4e11d0dab572e6022d54d227
rashaaad19 <egyoussef_2000@hotmail.com>
2025-06-02 14:25:39 +0300

    feat: Added players screen file

---
+++ lib/screens/players_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//TODO: https://apiv2.allsportsapi.com/football/?&met=Players&teamId=85&APIkey=b7d207bbb75cf6b51de0aebc8ca55c8412b7c5a33c77510be5338170718b911d
+
+class PlayersScreen extends StatefulWidget {
+  const PlayersScreen({super.key});
+
+  @override
+  State<PlayersScreen> createState() => _PlayersScreenState();
+}
+
+class _PlayersScreenState extends State<PlayersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
