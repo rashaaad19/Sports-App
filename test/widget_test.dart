3649612dfce927b4812f24999be5217cd1c7b1fa
rashaaad19 <egyoussef_2000@hotmail.com>
2025-06-05 00:04:47 +0300

    fix: error at the test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sportsapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(isFirstTime: true));
+    await tester.pumpWidget(const MyApp(isFirstTime: true, user: null));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
